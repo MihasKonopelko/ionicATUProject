3e40d0b7cb64e8410b5a2e574b266015ebde516b
Mihass Konopelko <mihapelko93@gmail.com>
2024-12-25 13:16:30 +0000

    - Implement Weather page. Described functionality is implemented.  Time to address bugs.

--- G00439360/src/app/weather/weather.page.ts
+++ G00439360/src/app/weather/weather.page.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonCard, IonCardHeader, IonCardSubtitle, IonCardTitle } from '@ionic/angular/standalone';
 
 import { DataManagerService } from '../services/data-manager.service';
 import { HttpManagerService } from '../services/http-manager.service';
@@ -11,22 +11,25 @@ import { HttpManagerService } from '../services/http-manager.service';
   templateUrl: './weather.page.html',
   styleUrls: ['./weather.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonCardTitle, IonCardSubtitle, IonCardHeader, IonCard, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
 })
 export class WeatherPage implements OnInit {
-  weatherData:any;
   apiKey!:string
+  
+  capitalName!:string
+  temperature!:number
+  weatherTitle!:string
+  urlToWeatherImage!:string;
+  selectedUnits!:string;
 
 
   constructor(private dms: DataManagerService, private hms: HttpManagerService) { }
 
   ngOnInit() {
-    this.weatherData = [];
     this.apiKey = "0bc224e47cade816d9380158370217d1";
   }
 
   ionViewWillEnter() {
-    this.weatherData = [];
     this.getWeather();
   }
 
@@ -36,25 +39,24 @@ export class WeatherPage implements OnInit {
     console.log (latlngToSearch);
 
     // Get Units Data
-    let selectedUnits = await this.dms.get("selectedUnits");
+    this.selectedUnits = await this.dms.get("selectedUnits");
 
+    // Get Capital Name
+    this.capitalName = await this.dms.get("capitalOfSearchedCountry");
 
-    // Create URL.
+    // Create URL to get weather data.
     let url = 
     "https://api.openweathermap.org/data/2.5/weather?lat=" + latlngToSearch[0] + 
     "&lon=" + latlngToSearch[1] + 
-    "&units=" + selectedUnits + 
+    "&units=" + this.selectedUnits + 
     "&appid=" + this.apiKey;
     
     let result:any = await this.hms.get({url:url});
-    
-    console.log (result.data);
+    this.temperature = result.data.main.temp;
+    this.weatherTitle = result.data.weather[0].description;
 
-   
-
-    
+    // Construct URL to an image for weather.
+    this.urlToWeatherImage = "https://openweathermap.org/img/wn/" + 
+      result.data.weather[0].icon + ".png";
   }
-
-
-
 }
