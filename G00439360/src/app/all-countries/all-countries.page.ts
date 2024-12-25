1a858522b1e9366e845746132df338c88b3ecc4b
Mihass Konopelko <mihapelko93@gmail.com>
2024-12-25 13:37:17 +0000

    - Altered transition to another page from All Countries Page: Now it uses Router within Typescript instead of routerLink in html.

--- G00439360/src/app/all-countries/all-countries.page.ts
+++ G00439360/src/app/all-countries/all-countries.page.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonContent, IonHeader, IonTitle, IonToolbar, IonCard, IonCardHeader, IonCardTitle, IonCardContent, IonButton } from '@ionic/angular/standalone';
-import { RouterLink } from '@angular/router';
+import { Router } from '@angular/router';
 import { HttpOptions } from '@capacitor/core';
 
 import { DataManagerService } from '../services/data-manager.service';
@@ -24,8 +24,7 @@ import { HttpManagerService } from '../services/http-manager.service';
     IonTitle, 
     IonToolbar, 
     CommonModule, 
-    FormsModule,
-    RouterLink]
+    FormsModule]
 })
 export class AllCountriesPage implements OnInit {
   countryToSearch!:string;
@@ -34,7 +33,7 @@ export class AllCountriesPage implements OnInit {
   newsLink!:string;
   weatherLink!:string;
 
-  constructor(private dms: DataManagerService, private hms: HttpManagerService) { }
+  constructor(private dms: DataManagerService, private hms: HttpManagerService, private router: Router) { }
 
 
   ngOnInit() {
@@ -67,10 +66,13 @@ export class AllCountriesPage implements OnInit {
     this.countryData = result.data;
   }
 
-  async onPressingButton(country:any){
+  async onPressingButton(country:any, nextPage:string){
     await this.dms.set("countryToSearch", country.name.official);
     await this.dms.set("cca2ToSearch", country.cca2);
     await this.dms.set("latlngToSearch", JSON.stringify(country.latlng));
     await this.dms.set("capitalOfSearchedCountry", country.capital[0]);
+
+    this.router.navigate([nextPage]);
+
   }
 }
