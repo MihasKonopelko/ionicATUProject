8533266acb75759d0779af101c86760c9e2b03ef
Mihass Konopelko <mihapelko93@gmail.com>
2024-12-25 10:36:56 +0000

    - Add logic to allCountries page to display flag, official country name and buttons for news and weather.

--- G00439360/src/app/all-countries/all-countries.page.ts
+++ G00439360/src/app/all-countries/all-countries.page.ts
@@ -1,33 +1,80 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonCard, IonCardHeader, IonCardTitle, IonCardContent, IonButton } from '@ionic/angular/standalone';
+import { RouterLink } from '@angular/router';
+import { HttpOptions } from '@capacitor/core';
 
 import { DataManagerService } from '../services/data-manager.service';
+import { HttpManagerService } from '../services/http-manager.service';
 
 @Component({
   selector: 'app-all-countries',
   templateUrl: './all-countries.page.html',
   styleUrls: ['./all-countries.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [
+    IonButton, 
+    IonCardContent, 
+    IonCardTitle, 
+    IonCardHeader, 
+    IonCard, 
+    IonContent, 
+    IonHeader, 
+    IonTitle, 
+    IonToolbar, 
+    CommonModule, 
+    FormsModule,
+    RouterLink]
 })
 export class AllCountriesPage implements OnInit {
   countryToSearch!:string;
+  countryData:any;
 
-  constructor(private dms: DataManagerService) { }
+  newsLink!:string;
+  weatherLink!:string;
+
+  constructor(private dms: DataManagerService, private hms: HttpManagerService) { }
+
 
   ngOnInit() {
-    this.getCountryToSearchFromStorage();
+    this.newsLink = "/news";
+    this.weatherLink = "/weather";
+
+    this.countryToSearch = "";
+    this.getCountries();
   }
 
   ionViewWillEnter() {
     
   }
 
-  async getCountryToSearchFromStorage() {
-    this.countryToSearch = await this.dms.get("countryToSearch");
+
+  async getCountryToSearchFromStorageAndFromInternet() {
+    
   }
 
+  async getCountries(){
+    // Wait to get name from DB.
+    this.countryToSearch = await this.dms.get("countryToSearch");
 
+    // Create URL.
+    let url = "https://restcountries.com/v3.1/name/" + this.countryToSearch;
+
+    // Wait to get countries from internet.
+    let result:any = await this.hms.get({url:url});
+
+
+    this.countryData = result.data;
+
+
+    //for (let entry of result.data){
+    //  console.log(entry.name.official);
+    //}
+
+
+
+
+
+  }
 }
