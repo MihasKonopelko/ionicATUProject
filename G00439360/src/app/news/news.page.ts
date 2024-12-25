001d3f5c35215326ac4a486eb4efface71bc9db4
Mihass Konopelko <mihapelko93@gmail.com>
2024-12-25 12:11:56 +0000

    - Add a separate title for news page when there are no news. - Handle empty news result from newsdata.

--- G00439360/src/app/news/news.page.ts
+++ G00439360/src/app/news/news.page.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonCard, IonCardHeader, IonButton, IonCardContent, IonCardTitle, IonCardSubtitle } from '@ionic/angular/standalone';
 
 import { DataManagerService } from '../services/data-manager.service';
 import { HttpManagerService } from '../services/http-manager.service';
@@ -11,20 +11,23 @@ import { HttpManagerService } from '../services/http-manager.service';
   templateUrl: './news.page.html',
   styleUrls: ['./news.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonCardSubtitle, IonCardTitle, IonCardContent, IonButton, IonCardHeader, IonCard, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
 })
 export class NewsPage implements OnInit {
   apiKey!:string;
   newsData:any;
+  country!:string;
 
   constructor(private dms: DataManagerService, private hms: HttpManagerService) { }
 
   ngOnInit() {
+    this.newsData = [];
     this.apiKey = "pub_632977eb474ea6d015a8d26530431158a7318";
   }
 
 
   ionViewWillEnter() {
+    this.newsData = [];
     this.getNews();
   }
 
@@ -32,6 +35,8 @@ export class NewsPage implements OnInit {
   async getNews(){
     // Wait to get name from DB.
     let cca2ToSearch = await this.dms.get("cca2ToSearch");
+    this.country = await this.dms.get("countryToSearch");
+
 
     // Create URL.
     let url = "https://newsdata.io/api/1/latest?apikey=" + this.apiKey + 
@@ -39,9 +44,15 @@ export class NewsPage implements OnInit {
 
     // Wait to get news for a country from internet.
     let result:any = await this.hms.get({url:url});
-    this.newsData = result.data.results;
 
-    console.log (result.data);
+    console.log (result.data.status);
+    if (result.data.status == "success"){
+      this.newsData = result.data.results;
+    }
+    console.log (result);
+    
+
+    
   }
 
 
